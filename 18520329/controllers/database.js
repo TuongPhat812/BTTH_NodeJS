89f47dc2788b69be7929f6490c263a4205ce9624
Phat Nguyen <tuongphat208@gmail.com>
2021-07-17 21:25:06 +0700

    BTTH Nodejs

---
+++ 18520329/controllers/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/qlbanbong', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+var conn = mongoose.connection;
+conn.on('connected', function(){
+    console.log('database is connected successfully');
+});
+conn.on('disconnected', function(){
+    console.log('database is disconnected successfully');
+});
+conn.on('error', console.error.bind(console, 'connection error:'));
+module.exports = conn;
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Bước 1. Tải và mở folder 18520329
+Bước 2. Tạo 1 csdl trong MongoDBCompass tại local host với tên là "qlbanbong"
+Bước 3. Tạo 3 Collection với tên lần lượt là : "hoas","khachhangs","loaihoas"
+Bước 4. Import data tại hoa.json vào hoas, khachhang.json vào khachhangs, loaihoa.json vào loaihoas
+Bước 5. Run terminal : node main 
+Nếu màn hình hiện ra dòng text : "database is connected successfully" nghĩa là file đã chạy thành công và truy cập vào http://localhost:8080/
+
